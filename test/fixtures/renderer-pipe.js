bfbfb8c3a8204ba25df575910df707e7ddb92d42
Geordan Gesink <georgegeordan@gmail.com>
2025-09-04 18:26:21 +0200

    use pear-pipe to test

--- test/fixtures/renderer-pipe.js
+++ test/fixtures/renderer-pipe.js
@@ -9,23 +9,25 @@ global.process = {
 }
 
 // Mock the electron renderer ipc.pipe()
-delete require.cache[require.resolve('which-runtime')]
 const { isBare } = require('which-runtime')
 const Pipe = isBare
   ? require('bare-pipe')
   : class Pipe extends require('net').Socket { constructor (fd) { super({ fd }) } }
 
-const pipe = new Pipe(3)
+const pipeMock = new Pipe(3)
 
 if (!global.Pear) global.Pear = {}
 if (!global.Pear.constructor) global.Pear.constructor = {}
 if (!global.Pear.constructor.IPC) global.Pear.constructor.IPC = Symbol('ipc')
+if (!global.Pear.exit) global.Pear.exit = () => pipeMock.destroy()
 
 global.Pear[global.Pear.constructor.IPC] = {
   pipe () {
-    return pipe
+    return pipeMock
   }
 }
+
+const pipe = require('../..')()
 
 // echo
 pipe.on('data', (data) => pipe.write(data))
