27421fc454d8939ca9bd540c40585e61c1a647ab
Geordan Gesink <georgegeordan@gmail.com>
2025-09-10 19:44:54 +0200

    lint

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { isBareKit, isPear, isMobile, isBare, isNode } = require('which-runtime')
 
 if (isBareKit || isMobile) {
   module.exports = require('./lib/bare-kit')
-}
-else if (isPear || isBare || isNode) {
+} else if (isPear || isBare || isNode) {
   module.exports = require('./lib/pear')
 }
