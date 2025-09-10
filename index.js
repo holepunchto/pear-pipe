62db1ae77f88952683005b0a63bf946f74944bc5
Geordan Gesink <georgegeordan@gmail.com>
2025-09-10 19:43:59 +0200

    also check for isBare and isNode

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { isBareKit, isPear, isMobile } = require('which-runtime')
+const { isBareKit, isPear, isMobile, isBare, isNode } = require('which-runtime')
 
-if (isPear) {
-  module.exports = require('./lib/pear')
-} else if (isBareKit || isMobile) {
+if (isBareKit || isMobile) {
   module.exports = require('./lib/bare-kit')
+}
+else if (isPear || isBare || isNode) {
+  module.exports = require('./lib/pear')
 }
