09292b4d998ded5a1565ad1056ae6e83ba544a13
Geordan Gesink <georgegeordan@gmail.com>
2025-09-12 00:40:18 +0200

    rm comments

--- test/helper.js
+++ test/helper.js
@@ -2,14 +2,7 @@
 
 const { isBare } = require('which-runtime')
 
-/**
- * Helper class to require the correct pipe module based on runtime environment
- */
 class Helper {
-  /**
-   * Require the correct pipe module based on runtime environment
-   * @returns {Function} The pipe function
-   */
   static requirePipe () {
     return isBare ? require('..') : require('../index.default')
   }
