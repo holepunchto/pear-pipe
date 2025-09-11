70cb7001fc7fa816a4b22bf118977e3a8a3223fc
Geordan Gesink <georgegeordan@gmail.com>
2025-09-11 20:26:44 +0200

    import conditions

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const { isBareKit, isPear, isMobile, isBare, isNode } = require('which-runtime')
-
-if (isBareKit || isMobile) {
-  module.exports = require('./lib/bare-kit')
-} else if (isPear || isBare || isNode) {
-  module.exports = require('./lib/pear')
-}
+module.exports = require('#lib')
