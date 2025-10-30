43ad716e5f6332e0b146d2351dd872cf8026ae3c
Geordan Gesink <georgegeordan@gmail.com>
2025-10-30 01:06:14 +0100

    fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict'
-const { isWindows, isBareKit, isPear, isMobile } = require('which-runtime')
+const { isWindows, isBareKit, isPear, isIOS, isAndroid, isReactNative, isExpo } = require('which-runtime')
 const fs = require('fs')
 const FD = 3
 const Pipe = require('bare-pipe')
+const isMobile = isIOS || isAndroid || isReactNative || isExpo
 
 class PearPipe extends Pipe {
   #onexit () { isPear && global.Pear.exit() }
@@ -29,9 +30,10 @@ class ThreadPipe {
   #writePipe
   
   constructor () {
-    const data = global.Bare.Thread.self.data
-    this.#readPipe = new Pipe(data.readFd)
-    this.#writePipe = new Pipe(data.writeFd)
+    const data = global.Bare?.Thread?.self?.data ?? null
+    if (data === null) throw new Error('Bare thread data should hold FDs')
+    this.#readPipe = new Pipe(data._readFd)
+    this.#writePipe = new Pipe(data._writeFd)
   }
 
   on(event, callback) {
@@ -58,7 +60,7 @@ class ThreadPipe {
 if (isBareKit) exports.args = [...Bare.argv]
 else if (!isPear) exports.args = Bare.argv.slice(2)
 
-let PIPE = (isMobile && !isReactNative) ? new ThreadPipe() : global.BareKit?.IPC ?? null
+let PIPE = isMobile ? global.BareKit?.IPC ?? new ThreadPipe() : null
 module.exports = function pipe () {
   if (PIPE !== null) return PIPE
   let attached
